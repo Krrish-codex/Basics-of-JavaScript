08008480f48b6d22052338805b835cf3da98ae76
Chetanya Mathur <mathurchetanya1@gmail.com>
2024-10-30 17:16:40 +0530

    added basics of javascript

---
+++ basicsOfJavascript.js
@@ -0,0 +1,25 @@
+let x = 7;
+const a = 3;
+// Make variables of Different datatype
+let y = 1234;
+let b = "krrishna123";
+let c = true;
+let d = null;
+let e = undefined;
+let f = { firstname: "krrishna", lastname: "kanojia", age: 23 };
+let g = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(y);
+console.log(typeof y);
+console.log(b);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+console.log(d);
+console.log(typeof d);
+console.log(e);
+console.log(typeof e);
+console.log(f);
+console.log(typeof f);
+console.log(g);
+console.log(typeof g)
